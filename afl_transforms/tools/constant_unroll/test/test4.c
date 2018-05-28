55b5157644247ba2fd7d77f07fa7054ef368c575
Anh <zenpoems@gmail.com>
2018-05-28 00:44:07 +0000

    Use different constant

--- afl_transforms/tools/constant_unroll/test/test4.c
+++ afl_transforms/tools/constant_unroll/test/test4.c
@@ -8,7 +8,10 @@ int main(int argc, char **argv)
 	int x;
 
 	read(0, &x, 4);
-	if (x == 29535)
+//	if (x == 33620225) // 0x02010101
+//	if (x == 3791716609) // 0xe2010101
+//	if (x == 16843057) 
+	if (x == 16851249) 
             abort();
 	return 0;
 }
