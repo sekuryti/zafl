aaa33de25fa5bc03a5fc8a357be4fbbef1f2e642
Anh Nguyen-Tuong <zenpoems@gmail.com>
2018-05-31 18:36:15 -0700

    Update tests

--- afl_transforms/tools/constant_unroll/test/test2.c
+++ afl_transforms/tools/constant_unroll/test/test2.c
@@ -1,17 +1,12 @@
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	int x = strtoul(argv[1], NULL, 16);
-
-	printf("x = 0x%x\n", x);
-	if (x >> 24 == 0x12)
-		if (((x&0xff0000) >> 16) == 0x34)
-			if (((x&0xff00) >> 8) == 0x56)
-				if ((x & 0xff) == 0x78)
-					return 1;
-
+	int x = 0;
+	std::cin >> std::hex >> x;
+	if (x != 0x01000000)
+            abort();
 	return 0;
 }
