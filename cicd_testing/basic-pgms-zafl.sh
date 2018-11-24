e527c7e2b4f75e54836d7c23f10dfd04fc03ec7e
Anh <zenpoems@gmail.com>
2018-11-24 08:49:33 -0800

    Add more Zafl configs to tests

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -4,5 +4,5 @@ set -x
 
 cd /tmp/zafl_test
 source set_env_vars
-cd $PEASOUP_HOME/tests; make clean; ./test_cmds.sh -c zafl
+cd $PEASOUP_HOME/tests; make clean; ./test_cmds.sh -c "zafl zafl_ida zafl_nostars zafl_opt_graph"
 
