ce70baab4104871612ebf94da5cfe4dc49883f52
Anh <zenpoems@gmail.com>
2018-11-24 10:08:38 -0800

    Slightly less aggressive tests

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -4,5 +4,11 @@ set -x
 
 cd /tmp/zafl_test
 source set_env_vars
-cd $PEASOUP_HOME/tests; make clean; ./test_cmds.sh -c "zafl zafl_ida zafl_nostars zafl_opt_graph"
+cd $PEASOUP_HOME/tests; make clean; 
+
+# test zafl config across all default apps
+./test_cmds.sh -c "zafl"
+
+# test other zafl configs on various apps
+./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph" -a "tar tcpdump bzip2"
 
