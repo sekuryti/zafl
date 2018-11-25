af5c93e91ec0941e3500077b3ea8366b1d01cb3a
Anh <zenpoems@gmail.com>
2018-11-24 16:12:49 -0800

    Use afl core pattern

--- cicd_testing/afl-zafl.sh
+++ cicd_testing/afl-zafl.sh
@@ -7,8 +7,15 @@ source set_env_vars
 
 # Test with afl
 echo "Core pattern settings"
-cat /proc/sys/kernel/core_pattern
+save_core_pattern=/tmp/$(whoami).core_pattern
+cat /proc/sys/kernel/core_pattern > $save_core_pattern
+
+echo "Setup core pattern for afl"
+$ZAFL_HOME/zfuzz/afl_setup_core_pattern.sh
 
 $ZAFL_HOME/zfuzz/test/strings/test_strings.sh
 
+echo "Restore original core pattern"
+cat $save_core_pattern > /proc/sys/kernel/core_pattern
 
+rm $save_core_pattern
