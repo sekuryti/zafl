f4c66222cd60cd20645af318a75a76195f2e1388
Anh <zenpoems@gmail.com>
2018-11-24 16:18:43 -0800

    Use sudo to overwrite core pattern file

--- cicd_testing/afl-zafl.sh
+++ cicd_testing/afl-zafl.sh
@@ -8,7 +8,7 @@ source set_env_vars
 # Test with afl
 echo "Core pattern settings"
 save_core_pattern=/tmp/$(whoami).core_pattern
-cat /proc/sys/kernel/core_pattern > $save_core_pattern
+sudo cat /proc/sys/kernel/core_pattern > $save_core_pattern
 
 echo "Setup core pattern for afl"
 $ZAFL_HOME/zfuzz/util/afl_setup_core_pattern.sh
@@ -16,6 +16,6 @@ $ZAFL_HOME/zfuzz/util/afl_setup_core_pattern.sh
 $ZAFL_HOME/zfuzz/test/strings/test_strings.sh
 
 echo "Restore original core pattern"
-cat $save_core_pattern > /proc/sys/kernel/core_pattern
+sudo cat $save_core_pattern > /proc/sys/kernel/core_pattern
 
 rm $save_core_pattern
