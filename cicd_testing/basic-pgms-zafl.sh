e939e0b7f297421988bf201118b669e99d04402a
Anh <zenpoems@gmail.com>
2018-12-11 13:59:18 -0800

    Add sanity checks for kill_deads as zafl relies on kill_deads

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -6,9 +6,12 @@ cd /tmp/zafl_test
 source set_env_vars
 cd $PEASOUP_HOME/tests; make clean; 
 
+# test kill_deads as zafl relies on it
+./test_cmds.sh -c "kill_deads" -a "bzip2 tar tcpdump" -l
+
 # test zafl config across all default apps
 ./test_cmds.sh -c "zafl" -l
 
 # test other zafl configs on various apps
-./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph" -a "tar tcpdump bzip2" -l
+./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph" -a "bzip2 tar tcpdump" -l
 
