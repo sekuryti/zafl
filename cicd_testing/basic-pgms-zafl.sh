e4087369ced6fc5bd26483e13bac3457f6df0da8
Anh <zenpoems@gmail.com>
2018-12-13 10:49:25 -0800

    Use different tests for NIGHTLY builds

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -9,9 +9,14 @@ cd $PEASOUP_HOME/tests; make clean;
 # test kill_deads as zafl relies on it
 ./test_cmds.sh -c "kill_deads" -a "bzip2 tar tcpdump" -l
 
-# test zafl config across all default apps
-./test_cmds.sh -c "zafl" -l
+if [[ $CICD_NIGHTLY == 1 ]] ; then
 
-# test other zafl configs on various apps
-./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph" -a "bzip2 tar tcpdump" -l
+	# test zafl config across all default apps
+	./test_cmds.sh -c "zafl" -l
 
+	# test other zafl configs on various apps
+	./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph" -a "bzip2 tar tcpdump" -l
+
+else
+	./test_cmds.sh -c "zafl" -a "bzip2 tar" -l
+fi
