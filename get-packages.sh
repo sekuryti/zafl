2f54e6a0737e78cc667eb26c7e5af5ecaa8c05ef
zp <zp@localhost.localdomain>
2019-01-02 11:51:01 -0500

    Fix for CentOS

--- get-packages.sh
+++ get-packages.sh
@@ -1,6 +1,16 @@
-PKGS="libtool glib2.0-dev texinfo libtool-bin"
+which apt-get >/dev/null 2>/dev/null
+if [ $? -eq 0 ]; then
+	PKGS="libtool glib2.0-dev texinfo libtool-bin"
+else
+	PKGS="libtool glib2.0-dev texinfo"
+fi
 
 for i in $PKGS
 do
-	sudo apt-get install $i -y --force-yes
+	which apt-get >/dev/null 2>/dev/null
+	if [ $? -eq 0 ]; then
+		sudo apt-get install $i -y --force-yes
+	else
+		sudo yum install $i -y --skip-broken
+	fi
 done
