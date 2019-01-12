d5155860a11d4306eda42474b60e0bfdcba91fa6
Anh <zenpoems@gmail.com>
2019-01-12 15:51:31 -0800

    Add ncal to test

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -15,7 +15,7 @@ if [[ $CICD_NIGHTLY == 1 ]] ; then
 	./test_cmds.sh -c "zafl" -l
 
 	# test other zafl configs on various apps
-	./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph zafl_untracer" -a "bzip2 tar tcpdump" -l
+	./test_cmds.sh -c "zafl_ida zafl_nostars zafl_opt_graph zafl_untracer" -a "bzip2 tar tcpdump ncal" -l
 
 else
 	./test_cmds.sh -c "zafl zafl_untracer" -a "bzip2 tar" -l
