87f0128d8a8929402a0ae4302cf8d29b335c3323
Anh Nguyen <zenpoems@gmail.com>
2019-01-20 11:13:07 -0800

    Cleanup install

--- get-packages.sh
+++ get-packages.sh
@@ -1,3 +1,12 @@
+#
+# skeleton get-packages.sh
+#
+
+#
+# do not need any additional packages above and beyond Zipr toolchain
+# those packages will be installed separately
+#
+
 args="$@"
 if [[ $args = "" ]]; then
         args="all"
@@ -12,23 +21,4 @@ for arg in $args; do
 		exit 1
 		;;
 	esac
-done
-
-# @todo: none of these are necessary?
-#        these were for building QEMU/afl but that's not a zfuzz concern
-which apt-get >/dev/null 2>/dev/null
-if [ $? -eq 0 ]; then
-	PKGS="libtool glib2.0-dev texinfo libtool-bin"
-else
-	PKGS="libtool glib2.0-dev texinfo"
-fi
-
-for i in $PKGS
-do
-	which apt-get >/dev/null 2>/dev/null
-	if [ $? -eq 0 ]; then
-		sudo apt-get install $i -y --force-yes
-	else
-		sudo yum install $i -y --skip-broken
-	fi
 done
--- util/setup-afl.sh
+++ util/setup-afl.sh
@@ -16,6 +16,9 @@ if [ ! -e afl ]; then
 	make
 #	cd qemu_mode && ./build_qemu_support.sh
 
+	AFL_PATH=/home/tester/zafl_umbrella/zfuzz/afl
+	export AFL_PATH
+
 	# afl wants this
 	sudo $ZFUZZ_HOME/util/afl_setup_core_pattern.sh
 fi
