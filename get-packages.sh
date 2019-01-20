359c6468ae1299716526583f61b5e8ffbde40ad7
Anh <zenpoems@gmail.com>
2019-01-20 10:48:10 -0800

    Cleanup install script

--- get-packages.sh
+++ get-packages.sh
@@ -1,3 +1,21 @@
+args="$@"
+if [[ $args = "" ]]; then
+        args="all"
+fi
+
+for arg in $args; do
+	case $arg in
+		all | build | test | deploy)
+			;;
+		*)
+		echo "arg not recognized. Recognized args: all, build, test, deploy"
+		exit 1
+		;;
+	esac
+done
+
+# @todo: none of these are necessary?
+#        these were for building QEMU/afl but that's not a zfuzz concern
 which apt-get >/dev/null 2>/dev/null
 if [ $? -eq 0 ]; then
 	PKGS="libtool glib2.0-dev texinfo libtool-bin"
