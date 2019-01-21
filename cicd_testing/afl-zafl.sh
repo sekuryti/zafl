4d76c8bc53e3c0ea5c5c826dcf2194a9dab203c9
Anh <zenpoems@gmail.com>
2019-01-20 17:03:14 -0800

    Use sudo -E

--- cicd_testing/afl-zafl.sh
+++ cicd_testing/afl-zafl.sh
@@ -10,9 +10,9 @@ echo "Core pattern settings"
 save_core_pattern=/tmp/$(whoami).core_pattern
 sudo cat /proc/sys/kernel/core_pattern > $save_core_pattern
 
-echo "Setup core pattern for afl - ZAFL_HOME=$ZAFL_HOME"
-sudo $ZAFL_HOME/zfuzz/util/setup-afl.sh
-sudo $ZAFL_HOME/zfuzz/util/afl_setup_core_pattern.sh
+echo "Setup afl - ZAFL_HOME=$ZAFL_HOME ZFUZZ_HOME=$ZFULL_HOME"
+sudo -E $ZAFL_HOME/zfuzz/util/setup-afl.sh
+sudo -E $ZAFL_HOME/zfuzz/util/afl_setup_core_pattern.sh
 
 echo "Test various zafl configurations"
 $ZAFL_HOME/zfuzz/test/strings/test_strings.sh
