4a0c1b1ef4a3cd4d5856860d575d50af7eccab24
Anh Nguyen-Tuong <zenpoems@gmail.com>
2019-02-23 16:21:32 -0500

    Update tests

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -16,7 +16,7 @@ main()
 	if [[ $CICD_NIGHTLY == 1 ]] ; then
 		export ZAFL_TRACE_MAP_FIXED_ADDRESS=0x10000
 		benchmarks="tcpdump ncal bzip2 tar"
-		configs="zafl_nostars zafl_opt_graph zafl_domgraph zafl_untracer_critical_edges zafl_fix_map"
+		configs="zafl_nostars zafl_opt_graph zafl_domgraph zafl_untracer_critical_edges zafl_context_sensitive"
 	else
 		benchmarks="readelf touch"
 		configs="zafl_domgraph zafl_context_sensitive"
