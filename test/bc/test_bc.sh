c83019ed3d439ee7aa7c356a759472562eb4c6a1
Anh Nguyen-Tuong <zenpoems@gmail.com>
2019-02-24 16:32:51 -0500

    Test with seed, context sensitivity and dom graph

--- test/bc/test_bc.sh
+++ test/bc/test_bc.sh
@@ -58,14 +58,14 @@ mkdir -p $session
 pushd $session
 
 # build with graph optimization
-zafl.sh `which bc` bc.stars.zafl.d.g -d -g --tempdir analysis.bc.stars.zafl.g
+zafl.sh `which bc` bc.stars.zafl.d.g.r.cs -d -g --tempdir analysis.bc.stars.zafl.d.g.r.cs -r 123 --enable-context-sensitivity function
 if [ $? -eq 0 ]; then
-	log_success "build bc.stars.zafl.d.g"
+	log_success "build bc.stars.zafl.d.g.r.cs"
 else
-	log_error "build bc.stars.zafl.d.g"
+	log_error "build bc.stars.zafl.d.g.r.cs"
 fi
 log_message "Fuzz for $AFL_TIMEOUT secs"
-fuzz_with_zafl $(realpath ./bc.stars.zafl.d.g)
+fuzz_with_zafl $(realpath ./bc.stars.zafl.d.g.r.cs)
 
 # build ZAFL version of readline shared library
 readline=$( ldd `which bc` | grep libreadline | cut -d'>' -f2 | cut -d'(' -f1 )
@@ -73,7 +73,7 @@ readline_basename=$( basename $readline )
 readline_realpath=$( realpath $readline )
 echo "basename: $readline_basename  realpath: $readline_realpath"
 #$PSZ $readline_realpath $readline_basename -c move_globals=on -c zafl=on -o move_globals:--elftables -o zipr:--traceplacement:on -o zipr:true -o zafl:--stars 
-zafl.sh $readline_realpath $readline_basename
+zafl.sh $readline_realpath $readline_basename --random-seed 42 -d
 if [ $? -eq 0 ]; then
 	log_success "build zafl version of $readline_basename at $readline_realpath"
 else
@@ -82,12 +82,12 @@ fi
 
 # test functionality
 echo "2+3" | `which bc` > out.bc.orig
-echo "2+3" | ./bc.stars.zafl.d.g > out.bc.stars.zafl.d.g
-diff out.bc.orig out.bc.stars.zafl.d.g >/dev/null 2>&1
+echo "2+3" | ./bc.stars.zafl.d.g.r.cs > out.bc.stars.zafl.d.g.r.cs
+diff out.bc.orig out.bc.stars.zafl.d.g.r.cs >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-	log_success "bc.stars.zafl.d.g basic functionality"
+	log_success "bc.stars.zafl.d.g.r.cs basic functionality"
 else
-	log_error "bc.stars.zafl.d.g basic functionality"
+	log_error "bc.stars.zafl.d.g.r.cs basic functionality"
 fi
 
 popd
