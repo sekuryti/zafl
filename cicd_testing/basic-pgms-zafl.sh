ea05813d0e454f455f66a547d61ed689e04d7111
Anh Nguyen-Tuong <zenpoems@gmail.com>
2019-03-19 15:56:58 -0400

    Reduce # zafl tests

--- cicd_testing/basic-pgms-zafl.sh
+++ cicd_testing/basic-pgms-zafl.sh
@@ -16,10 +16,10 @@ main()
 	if [[ $CICD_NIGHTLY == 1 ]] ; then
 		export ZAFL_TRACE_MAP_FIXED_ADDRESS=0x10000
 		benchmarks="tcpdump ncal bzip2 tar"
-		configs="zafl_nostars zafl_opt_graph zafl_untracer_critical_edges zafl_context_sensitive_laf_domgraph"
+		configs="zafl_nostars zafl_untracer_critical_edges zafl_context_sensitive_laf_domgraph_optgraph"
 	else
 		benchmarks="readelf touch"
-		configs="zafl_domgraph zafl_context_sensitive zafl_laf"
+		configs="zafl_context_sensitive zafl_laf_domgraph"
 	fi
 
 	# test other zafl configs on various apps
