06bc3429e7b52b1e6882533cf12c6605a23f4589
Anh Nguyen-Tuong <zenpoems@gmail.com>
2019-03-21 14:55:32 -0400

    Reduce # configs

--- test/functional/test_zafl_configs.sh
+++ test/functional/test_zafl_configs.sh
@@ -1,6 +1,6 @@
 echo "Test various zafl configs"
 
-zafl_configs="zafl zafl_context_sensitive_laf_domgraph_optgraph zafl_nostars zafl_laf_domgraph zafl_context_sensitive_laf "
+zafl_configs="zafl zafl_context_sensitive_laf_domgraph_optgraph zafl_nostars"
 
 pushd $PEASOUP_HOME/tests
 
