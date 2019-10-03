cceeb2594c221279ee052c4248a80ab981970653
Anh Nguyen-Tuong <zenpoems@gmail.com>
2019-10-03 13:25:44 -0400

    Update with proper SBIR data rights headers

--- tools/zax/loop_count.cpp
+++ tools/zax/loop_count.cpp
@@ -1,18 +1,36 @@
+// @HEADER_LANG C++ 
+// @HEADER_COMPONENT zafl
+// @HEADER_BEGIN
 /*
-   Copyright 2017-2019 University of Virginia
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+* Copyright (c) 2018-2019 Zephyr Software LLC
+*
+* This file may be used and modified for non-commercial purposes as long as
+* all copyright, permission, and nonwarranty notices are preserved.
+* Redistribution is prohibited without prior written consent from Zephyr
+* Software.
+*
+* Please contact the authors for restrictions applying to commercial use.
+*
+* THIS SOURCE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+* MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*
+* Author: Zephyr Software
+* e-mail: jwd@zephyr-software.com
+* URL   : http://www.zephyr-software.com/
+*
+* This software was developed with SBIR funding and is subject to SBIR Data Rights, 
+* as detailed below.
+*
+* SBIR DATA RIGHTS
+*
+* Contract No. __FA8750-17-C-0295___________________________.
+* Contractor Name __Zephyr Software LLC_____________________.
+* Address __4826 Stony Point Rd, Barboursville, VA 22923____.
+*
 */
+
+// @HEADER_END
 
 #include <assert.h>
 #include "loop_count.hpp"
